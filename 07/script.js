cb4b40b69483e61053858d5d75bc9eda74012d15
Ana_Ska <anna.skalabava@dizzain.com>
2016-12-28 17:42:30 +0300

    07

---
+++ 07/script.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+    // ## Array Cardio Day 2
+    const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 823423 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
+
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people.some( person => {
+    	return new Date().getFullYear() - person.year >= 19;
+    });
+    // console.log({isAdult});
+
+    // Array.prototype.every() // is everyone 19 or older?
+    const allAdult = people.every( person => {
+    	return new Date().getFullYear() - person.year >= 19;
+    });
+    // console.log({allAdult});
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    const findComment = comments.find( comment => {
+    	return comment.id === 823423;
+    });
+    // console.log({findComment});
+
+    // find the comment with the ID of 823423
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+    const findIndex = comments.findIndex( comment => {
+    	return comment.id === 823423;
+    });
+    console.log({findIndex});
+
+    // comments.splice(findIndex, 1);
+    // console.table(comments);
+
+    const copyComments = [
+    	...comments.slice(0, findIndex),
+    	...comments.slice(findIndex+1)
+    ];
+
+    console.table(copyComments);
+
+};
